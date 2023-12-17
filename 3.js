1afd79c8b5a5c45914f49353c9c3a0982e8f7f5a
Suhejl111 <154080009+Suhejl111@users.noreply.github.com>
2023-12-17 17:45:50 +0100

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,9 @@
+function checkLove() {
+    document.getElementById("result").innerText = "You are mine too!";
+}
+
+function makeSad() {
+    document.getElementById("result").innerText = "You make me sad.";
+}
+
+
